fa19163990b1eb75614d644916ccc93157fde599
WHITE DEVIL <whitedevilngey@gmail.com>
2025-04-18 16:48:33 +0530

    GetRating,LensSearch and About us page created

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import logo from './logo.svg';
 import ProductCard from './components/ProductCard';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-
+import GetRating from './pages/GetRating';
+import LensSearch from './pages/LensSearch';
+import AboutUs from './pages/AboutUs';
 function App() {
   return (
     <Router>  
     <Routes>
       <Route path="/" element={<Home />} />
-    
+      <Route path="/Get-Rating" element={<GetRating />} />
+      <Route path="/Lens-Search" element={<LensSearch />} />
+      <Route path="/About-us" element={<AboutUs />} />
     </Routes>
     </Router>
   );
